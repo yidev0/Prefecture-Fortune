96b8c30844044c67d9b7f6100dac1139a0c86a86
Yuto Iwakami <11906960+yidev0@users.noreply.github.com>
2024-09-25 12:22:21 +0900

    Create MainView

---
+++ Prefecture Fortune/View/Main/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Prefecture Fortune
+//
+//  Created by Yuto on 2024/09/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Form {
+            
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Prefecture Fortune/View/Main/MainViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewModel.swift
+//  Prefecture Fortune
+//
+//  Created by Yuto on 2024/09/25.
+//
+
+import Foundation
+
+@MainActor
+struct MainViewModel {
+    // MARK: Inputs
+    var name: String
+    var birthday: Date
+    var bloodType: BloodType
+    
+}
