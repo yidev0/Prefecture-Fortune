999e6b3cdf88b70255b1164502b6dfce6f74bdd7
Yuto Iwakami <11906960+yidev0@users.noreply.github.com>
2024-09-28 19:18:22 +0900

    Add model container

--- Prefecture Fortune/Prefecture_FortuneApp.swift
+++ Prefecture Fortune/Prefecture_FortuneApp.swift
@@ -14,6 +14,7 @@ struct Prefecture_FortuneApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: FortuneData.self)
         }
     }
     
