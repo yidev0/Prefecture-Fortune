abd2fd29ecbe9234c3a6b1b1faff53a96f2ebae9
Yuto Iwakami <11906960+yidev0@users.noreply.github.com>
2024-09-28 19:32:41 +0900

    Create list to show history

---
+++ Prefecture Fortune/View/History/HistoryView.swift
@@ -0,0 +1,41 @@
+//
+//  HistoryView.swift
+//  Prefecture Fortune
+//
+//  Created by Yuto on 2024/09/28.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HistoryView: View {
+    
+    @Environment(\.modelContext) private var context
+    
+    @Query
+    var fortuneData: [FortuneData]
+    
+    var body: some View {
+        let groupedItems = Dictionary(grouping: fortuneData) { data in
+            return data.date
+        }
+        
+        List {
+            ForEach(groupedItems.keys.sorted(), id: \.self) { key in
+                Section {
+                    ForEach(groupedItems[key] ?? []) { item in
+                    }
+                } header: {
+                    Text(
+                        key,
+                        format: .dateTime.year().month().day().weekday()
+                    )
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HistoryView()
+}
